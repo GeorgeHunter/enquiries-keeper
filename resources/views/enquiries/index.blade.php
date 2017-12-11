259e9d23dc38a5db73f57e09fb52a15060b20aa0
stellasoft-george <george@stellasoft.co.uk>
2017-12-11 09:17:07 +0000

    filter form updates

--- resources/views/enquiries/index.blade.php
+++ resources/views/enquiries/index.blade.php
@@ -10,7 +10,7 @@
                 <label class="sr-only" for="start-date">Start Date</label>
                 <div class="input-group mb-2 mb-sm-0">
                     <div class="input-group-addon"><i class="far fa-calendar-alt"></i></div>
-                    <input type="date" class="form-control" id="start-date" name="start-date" placeholder="Username" value="{{ request('start-date') }}">
+                    <input type="date" class="form-control datepicker-1" id="start-date" placeholder="yyyy/mm/dd" name="start-date" value="{{ request('start-date') }}">
                 </div>
 
                 <div class="mx-2">to</div>
@@ -18,7 +18,7 @@
                 <label class="sr-only" for="end-date">End Date</label>
                 <div class="input-group mb-2 mr-sm-2 mb-sm-0">
                     <div class="input-group-addon"><i class="far fa-calendar-alt"></i></div>
-                    <input type="date" class="form-control" id="end-date" name="end-date" placeholder="Username" value="{{ request('end-date') }}">
+                    <input type="date" class="form-control datepicker-2" id="end-date" placeholder="yyyy/mm/dd" name="end-date" value="{{ request('end-date') }}">
                 </div>
 
                 <div class="form-check">
@@ -28,7 +28,8 @@
                     </label>
                 </div>
             </div>
-            <button class="btn btn-primary btn-outline-primary" type="submit">Filter</button>
+            <button class="btn btn-outline-primary" type="submit">Filter</button>
+            <a href="/enquiries/" class="btn btn-outline-danger">Remove Filters</a>
         </form>
 
         <total-enquiries enquiries="{{ $dates }}"></total-enquiries>
